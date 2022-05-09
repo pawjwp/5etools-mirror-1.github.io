37a0fc988880db847ec4f47573a7e4b2b6800bd8
Isaac <isaac@rubenfamily.com>
2022-05-09 11:21:41 -0700

    continue work on compiling sw

--- node/build-sw.mjs
+++ node/build-sw.mjs
@@ -1,16 +1,21 @@
 import {injectManifest} from "workbox-build";
 import esbuild from "esbuild";
+import {writeFile} from "fs/promises";
 
-injectManifest({
+await writeFile("sw.js", "");
+
+const resp = await injectManifest({
 	swSrc: "sw-template.js",
 	swDest: "sw.js",
 	globDirectory: "/",
 	globPatterns: [
-		"js/**/*.js",
-		"css/**/*.css",
+		"/js/**/*.js",
+		"/css/**/*.css",
 		"*.html",
 	],
 });
+
+console.log(resp);
 
 console.log("manifest injected");
 
