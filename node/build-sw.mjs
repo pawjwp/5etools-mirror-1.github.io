a765bee757a2b9a124b7fe1309117f6074db8573
Isaac <isaac@rubenfamily.com>
2022-05-10 21:36:41 -0700

    much more complex precaching glob

--- node/build-sw.mjs
+++ node/build-sw.mjs
@@ -19,10 +19,18 @@ const workboxBuildResult = await injectManifest({
 	maximumFileSizeToCacheInBytes: 5 /* mb */ * 1e6,
 	globDirectory: "", // use the current directory - run this script from project root.
 	globPatterns: [
-		"js/**/*.js",
-		"css/**/*.css",
-		"data/**/*.json",
-		"*.html",
+		"js/**/*.js", // all js needs to be loaded
+		"lib/**/*.js", // js in lib needs to be loaded
+		"css/**/*.css", // all css needs to be loaded
+		"homebrew/**/*.json", // presumably if there is homebrew data it should also be loaded
+		// we want to match all data unless its for an adventure
+		"data/*.json", // root level data
+		"data/**/!(adventure)/*.json", // matches all json in data unless it is a file inside a directory called adventure
+		"*.html", // all html pages need to be loaded
+		"manifest.webmanifest", // we should make sure we have the manifest, although its not strictly needed...
+		// we want to store fonts to make things styled nicely
+		"fonts/glyphicons-halflings-regular.woff2",
+		"fonts/Convergence-Regular.ttf", // this should be a woff2 but that is its own pr
 	],
 });
 
