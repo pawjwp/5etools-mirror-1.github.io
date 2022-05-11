e2067f550bc4fe710761859699dd3e79fa5a9a2b
Isaac <isaac@rubenfamily.com>
2022-05-10 22:24:50 -0700

    add runtime caching for images and fonts

--- sw-template.js
+++ sw-template.js
@@ -1,4 +1,11 @@
 import { precacheAndRoute } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
+import { CacheFirst } from "workbox-strategies";
 
 // the self value is replaced with key: value pair of file: hash, to allow workbox to carry files over between caches if they match
 precacheAndRoute(self.__WB_MANIFEST);
+
+// this tells workbox to cache any of the selected types, and serve them cache first after first load
+// this works on the assumption that images and fonts are static assets and won't change
+const cacheTypes = new Set(["image", "font"]);
+registerRoute(({request}) => cacheTypes.has(request.destination), new CacheFirst());
