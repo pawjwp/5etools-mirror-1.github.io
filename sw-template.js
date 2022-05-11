743a0a103320a2efb7bfd2484905d8f065eff35d
Isaac <isaac@rubenfamily.com>
2022-05-11 10:59:38 -0700

    start work on revisioncachefirst strategy

--- sw-template.js
+++ sw-template.js
@@ -2,6 +2,8 @@ import { precacheAndRoute } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
 import { CacheFirst } from "workbox-strategies";
 
+import {Strategy, StrategyHandler} from "workbox-strategies";
+
 // the self value is replaced with key: value pair of file: hash, to allow workbox to carry files over between caches if they match
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -9,3 +11,14 @@ precacheAndRoute(self.__WB_MANIFEST);
 // this works on the assumption that fonts are static assets and won't change
 const cacheTypes = new Set(["font"]);
 registerRoute(({request}) => cacheTypes.has(request.destination), new CacheFirst());
+
+class RevisionCacheFirst extends Strategy {
+	/**
+   * @param {Request} request
+   * @param {StrategyHandler} handler
+   * @returns {Promise<Response | undefined>}
+   */
+	_handle (request, handler) {
+
+	}
+}
