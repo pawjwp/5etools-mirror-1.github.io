916db6f756fef90ff4b5a208c0474e5d3d1aabbe
Isaac <isaac@rubenfamily.com>
2022-05-12 20:06:56 -0700

    update comments to add info

--- sw-template.js
+++ sw-template.js
@@ -5,13 +5,15 @@ import { CacheFirst, Strategy, StrategyHandler } from "workbox-strategies";
 import { generateURLVariations } from "workbox-precaching/utils/generateURLVariations";
 import { createCacheKey } from "workbox-precaching/utils/createCacheKey";
 
+// routes take precedence in order listed. if a higher route and a lower route both match a file, the higher route will resolve it
+// https://stackoverflow.com/questions/52423473/workbox-routing-registerroute-idempotence
+
 // the self value is replaced with key: value pair of file: hash, to allow workbox to carry files over between caches if they match
 // precacheAndRoute(self.__WB_PRECACHE_MANIFEST);
 
-// this tells workbox to cache any of the selected types, and serve them cache first after first load
+// this tells workbox to cache fonts, and serve them cache first after first load
 // this works on the assumption that fonts are static assets and won't change
-const cacheTypes = new Set(["font"]);
-registerRoute(({request}) => cacheTypes.has(request.destination), new CacheFirst());
+registerRoute(({request}) => request === "font", new CacheFirst());
 
 class RevisionCacheFirst extends Strategy {
 	constructor () {
